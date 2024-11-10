63fd65c0cb3e2ea41eb00b4ca1d3527cb1477237
José Luis López G <lgjoseluis@hotmail.com>
2024-11-09 19:32:01 -0600

    Agregar proyecto inicial

---
+++ src/test/java/com/local/vacantes/ApiVacantesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.local.vacantes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiVacantesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
