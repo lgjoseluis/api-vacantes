54740a20e91375f26299c48f15d6b30a8ec19bc7
José Luis López G <lgjoseluis@hotmail.com>
2024-11-24 14:06:20 -0600

    Correcion de manejo de errores

--- src/main/java/com/local/vacantes/infrastructure/configuration/GlobalExceptionHandler.java
+++ src/main/java/com/local/vacantes/infrastructure/configuration/GlobalExceptionHandler.java
@@ -6,12 +6,14 @@ import java.util.stream.Collectors;
 
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,7 +22,7 @@ import jakarta.persistence.EntityNotFoundException;
 import com.local.vacantes.utils.ProblemDetails;
 
 @ControllerAdvice
-public class GlobalExceptionHandler /*extends ResponseEntityExceptionHandler*/ {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 	 private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     // Manejo de EntityNotFoundException, lanzada si no se encuentra un recurso
@@ -39,10 +41,13 @@ public class GlobalExceptionHandler /*extends ResponseEntityExceptionHandler*/ {
         return buildErrorResponse(ex, HttpStatus.BAD_REQUEST, request, "Argumento inválido");
     }
     
-    /*@Override
-    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, 
+    //NoResourceFoundException
+    
+    @Override    
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+    		MethodArgumentNotValidException ex, 
             HttpHeaders headers, 
-            HttpStatus status, 
+            HttpStatusCode status, 
             WebRequest request) {
 
         List<String> validationErrors = ex.getBindingResult()
@@ -60,25 +65,23 @@ public class GlobalExceptionHandler /*extends ResponseEntityExceptionHandler*/ {
         );
 
         return new ResponseEntity<>(apiError, HttpStatus.BAD_REQUEST);
-    }*/
+    }
     
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ProblemDetails> handleValidationExceptions(MethodArgumentNotValidException ex, WebRequest request) {
-        List<String> validationErrors = ex.getBindingResult()
-        		.getFieldErrors()
-        		.stream()
-                .map(FieldError::getDefaultMessage)
-                .collect(Collectors.toList());
+    @ExceptionHandler({ MethodArgumentTypeMismatchException.class })
+    public ResponseEntity<Object> handleMethodArgumentTypeMismatch(
+      MethodArgumentTypeMismatchException ex, WebRequest request) {    	
+        String error = ex.getName() + " debe ser de tipo " + ex.getRequiredType().getName();
+        
+        logger.error("{}: {}", ex.getName(), ex.getMessage(), ex);
 
         ProblemDetails apiError = new ProblemDetails(
-                HttpStatus.BAD_REQUEST.value(),
-                "Validation Error",
-                "Error en los datos de entrada",
-                request.getDescription(false),
-                validationErrors
-        );
-
-        return new ResponseEntity<>(apiError, HttpStatus.BAD_REQUEST);
+        		HttpStatus.BAD_REQUEST.value(), 
+        		HttpStatus.BAD_REQUEST.getReasonPhrase(), 
+        		error, 
+        		request.getDescription(false), 
+        		Collections.singletonList(ex.getLocalizedMessage()));
+        
+        return new ResponseEntity<Object>(apiError, new HttpHeaders(), apiError.getStatus());
     }
 
     // Manejo de excepciones genéricas
